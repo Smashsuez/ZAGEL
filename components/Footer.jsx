98bbb966d9761c78514d15a9c3d99621b11c9e47
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-10 04:09:24 +0300

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from "../styles/Footer.module.css";
-
+import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -13,13 +13,12 @@ const Footer = () => {
         </div>
         <div className={styles.right}>
           <ul className={styles.ul}>
-            <li>Home</li>
-            <li>Services</li>
-            <li>About</li>
-            <li>Clients</li>
-            <li>Contact</li>
+          <li><a href="/">Home</a></li>
+          <Link  legacyBehavior href={"/"} > <li><a href="#">Services</a></li></Link>
+          <Link  legacyBehavior href={"/"} > <li><a href="#">Feq Question</a></li></Link>
+          <Link  legacyBehavior href={"/"} > <li><a href="/about">About Us</a></li></Link>
           </ul>
-        </div>
+      </div>
       </div>
     </div>
     </div>
