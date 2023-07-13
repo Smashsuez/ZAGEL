ce3bcffec30508ac7884a6f0fb9e40a5493e842e
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-14 02:26:00 +0300

    google console

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home() {
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="google-site-verification" content="83LjU3ij1FIvWD7zMwXOt7CHlmUN-6cJuoHgcO0wGV8" />
         
       </Head>
       <Navbar/>
