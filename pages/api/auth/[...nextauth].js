c9fff1b6a21881571adaecc2874e39b7e5e280ec
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-13 20:39:39 +0300

    a

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google"
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: 621303710664-stn96klii9u57lkbdol3g5pj15m1p1tn.apps.googleusercontent.com,
-            clientSecret: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,
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ]})
