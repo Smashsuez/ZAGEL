ef8857c427b7b95e64b5625ad66bbbcd86d73f75
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-14 14:08:09 +0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import React, {useEffect} from 'react';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { SessionProvider } from "next-auth/react"
+import {signIn, useSession} from "next-auth/react"
 
 export default function App({ Component, pageProps: { session, ...pageProps }}) {
   useEffect(() => {
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth/next' 
 import GoogleProvider from "next-auth/providers/google"
-
+import {signIn, useSession} from "next-auth/react"
 export default NextAuth({
     providers: [
         GoogleProvider({
