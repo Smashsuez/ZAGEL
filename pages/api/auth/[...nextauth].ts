e675f6f67c3c89d33847b512829f450cc55167a1
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-16 13:26:47 +0300

    .

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"; // Updated import
+import { MongoClient } from "mongodb";
 import dbConnect from "../../../lib/dbConnect"
 import clientPromise from "../../../lib/mongodb"
 
