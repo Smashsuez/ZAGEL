22c7b23cf07f175750ccdc1bdefae048e2d76685
Smashsuez <mohamedahmedgameel@gmail.com>
2023-07-16 14:08:23 +0300

    .

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,6 @@
-import AboutUs from '@/components/AboutUs'
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
+import AboutUs from '../components/AboutUs'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import React from 'react'
 
 const about = () => {
